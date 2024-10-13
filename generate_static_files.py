a6eab982365c76bc3a68097cc412651fc99e1b12
Wieslaw Herdzik <wherdzik@gmail.com>
2024-10-13 08:23:18 -0700

    Remove unnecessary files from gh-pages branch

--- generate_static_files.py
+++
@@ -1,9 +0,0 @@
-from flask_frozen import Freezer
-from app import app
-
-app.config['FREEZER_STATIC_FILES'] = ['static/js/app.js']
-
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
